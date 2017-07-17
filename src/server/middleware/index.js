d11e20e5e5d40cf2aa96ef21e60f62cefaddf859
kathleendaily <kdaily@propelmarketing.com>
2017-07-17 11:06:41 -0400

    re-add ensureLoggedIn, remove validateUserSession

--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -41,6 +41,17 @@ export default class AbstractMiddleware {
  * @param  {[type]}   request  [description]
  * @param  {[type]}   response [description]
  * @param  {Function} next     [description]
+ * @return [type]              [description]
+ */
+const ensureLoggedIn = function(request: Object, response: Object, next: Function): void {
+
+}
+
+/**
+ * [description]
+ * @param  {[type]}   request  [description]
+ * @param  {[type]}   response [description]
+ * @param  {Function} next     [description]
  * @return {[type]}            [description]
  */
 const validateApiAccess = async function(request: Object, response: Object, next: Function): Promise<void> {
@@ -72,51 +83,5 @@ const validateApiAccess = async function(request: Object, response: Object, next
     error(e, response);
   }
 }
+export { ensureLoggedIn };
 export { validateApiAccess };
-
-
-/**
- * [description]
- * @param  {[type]} request  [description]
- * @param  {[type]} response [description]
- * @return {[type]}          [description]
- */
-const validateUserSession = function(request: Object, response: Object): void {
-  const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-  LOGGER.info(`[validateUserSession] Validating user session for ${ip}`);
-
-  // If the session was created piror to a password update/reset, then forcibly expire the session
-  if (request.session && request.user && (request.session.createTime < request.user.lastPasswordUpdate)) {
-    request.user = null;
-    return request.session.destroy(function (err) {
-      if (err) {
-        LOGGER.warn(`[validateUserSession] Failed to destroy session for invalid session: ${err}`);
-      }
-      response.clearCookie(sessionConfig.get('name'));
-      LOGGER.warn(`[validateUserSession] Connection attempt for ${ip} was blocked. The session is no longer valid.`);
-      return response.status(NOT_AUTHORIZED.status).send(NOT_AUTHORIZED);
-    });
-  }
-
-  if (request.user) {
-    const user = request.user;
-    // If the session is valid, then verify their client access levels
-    // If the user is allowed to access the client, then return the username
-    LOGGER.info(`[validateUserSession] Checking if user ${user.username} can access ${request.clientName} in (${user.applications})`)
-    if (user.applications.includes(request.clientName)) {
-      request.session.touch();
-      LOGGER.info(`[validateUserSession] ${user.username} is valid`);
-      return response.status(200).send({ username: user.username });
-    }
-
-    // Otherwise, permanently prevent user access
-    LOGGER.warn(
-      `[validateUserSession] Connection attempt for ${ip} was blocked. User not permitted to access the current client.`
-    );
-    return response.status(NOT_ALLOWED.status).send(NOT_ALLOWED);
-  }
-
-  LOGGER.warn(`[validateUserSession] Connection attempt for ${ip} was blocked. No valid user session was found.`);
-  return response.status(NOT_AUTHORIZED.status).send(NOT_AUTHORIZED);
-}
-export { validateUserSession };
