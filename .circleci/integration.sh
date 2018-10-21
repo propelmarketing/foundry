b794bb52b769e722e0d4d356da8f0ad143a139c1
Ken Jenney <kjenney@thrivehive.com>
2018-10-21 01:55:17 -0400

    Just refer to the parameter

--- .circleci/integration.sh
+++ .circleci/integration.sh
@@ -3,4 +3,4 @@
 # When the Unit tests pass this job is called by CircleCI in the workflow
 
 echo "Sending out for integration tests...beep boop beep"
-echo "When the integration tests are done somebody sends a slack message to Toadsworth who is turn calls the CircleCI API with $integration_passed"
+echo "When the integration tests are done somebody sends a slack message to Toadsworth who is turn calls the CircleCI API with parameter integration_passed"
